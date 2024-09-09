78bb308f4b911631e683d203625d6acbd7778382
Arman Mondal <arman.mondal.27012005@gmail.com>
2024-09-09 13:42:33 +0530

    new file:   .DS_Store 	new file:   .env 	modified:   index.js 	modified:   package-lock.json 	modified:   package.json 	new file:   prisma/migrations/20240819203918_init/migration.sql 	new file:   prisma/migrations/20240819204432_init/migration.sql 	new file:   prisma/migrations/20240819210412_init/migration.sql 	new file:   prisma/migrations/20240819210727_init/migration.sql 	new file:   prisma/migrations/migration_lock.toml 	new file:   prisma/schema.prisma

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const axios = require('axios');
-
+const { PrismaClient } = require('@prisma/client');
 const app = express();
 const cors = require('cors');
-
+const prisma = new PrismaClient()
+const jwt = require('jsonwebtoken');
 app.use(express.json());
 const corsOptions = {
     origin: ["*"],
@@ -64,6 +65,181 @@ app.post('/video', async (req, res) => {
         return res.status(500).json({ error: 'Something went wrong' });
     }
 });
+app.post('/register', async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        if (!email) {
+            return res.status(400).json({ error: 'Please provide the email' });
+        }
+        if (!password) {
+            return res.status(400).json({ error: 'Please provide the password' });
+        }
+
+        const user = await prisma.user.create({
+            data: {
+                name:'Admin',
+                email,
+                password
+            }
+        });
+
+        return res.json({ message: 'User created successfully',user });
+
+
+        
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ error: 'Something went wrong' });
+    }
+});
+app.post('/login', async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        if (!email) {
+            return res.status(400).json({ error: 'Please provide the email' });
+        }
+        if (!password) {
+            return res.status(400).json({ error: 'Please provide the password' });
+        }
+
+        const user = await prisma.user.findFirst({
+            where: {
+                email,
+                password
+            },
+            
+        });
+        if(!user){
+            return res.status(400).json({ error: 'Invalid email or password' });
+        }
+        const token=jwt.sign({email,password,id:user.id},'ArmanMondal',{expiresIn:'1h'});
+        const userdata=await prisma.user.update({
+            where: {
+                email,
+                password
+            },
+            data: {
+                token:token
+            }
+        });
+
+
+        if (!user) {
+            return res.status(400).json({ error: 'Invalid email or password' });
+        }
+
+        return res.json({ message: 'User logged in successfully', userdata });
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ error: 'Something went wrong' });
+    }
+}
+);
+app.get('/sync', async (req, res) => {
+    try {
+        const tokenData=req.headers.authorization;
+        const token=tokenData.split(' ')[1];
+        const decoded=jwt.verify(token,'ArmanMondal');
+        const userId=decoded.id;
+        if(!userId){
+            return res.status(400).json({ error: 'Please provide the user ID' });
+        }
+        const user = await prisma.user.findFirst({
+            where:{
+                id:userId
+            }
+        });
+        if(!user){
+            return res.status(400).json({ error: 'Invalid user' });
+        }
+
+
+        return res.json({ message: 'Database connected successfully', user });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ error: 'Something went wrong' });
+    }
+}
+);
+app.get('/users', async (req, res) => {
+    try {
+        const users = await prisma.user.findMany();
+        return res.json(users);
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ error: 'Something went wrong' });
+    }
+});
+app.get('/test',async(req,res)=>{
+    const tokenData=req.headers.authorization;
+    const token=tokenData.split(' ')[1];
+    const decoded=jwt.verify(token,'ArmanMondal');
+    console.log(decoded);
+    res.json({message:'Hello World',decoded});
+}
+)
+app.post('/create-post', async (req, res) => {
+    try {
+        const { title, content } = req.body;
+        const tokenData=req.headers.authorization;
+        const token=tokenData.split(' ')[1];
+        const decoded=jwt.verify(token,'ArmanMondal');
+        const userId=decoded.id;
+        if(!userId){
+            return res.status(400).json({ error: 'Please provide the user ID' });
+        }
+        
+
+
+        if (!title) {
+            return res.status(400).json({ error: 'Please provide the title' });
+        }
+        if (!content) {
+            return res.status(400).json({ error: 'Please provide the content' });
+        }
+        if (!userId) {
+            return res.status(400).json({ error: 'Please provide the user ID' });
+        }
+
+        const post = await prisma.post.create({
+            data: {
+                title,
+                content,
+                authorId: userId
+            }
+        });
+
+        return res.json({ message: 'Post created successfully', post });
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ error: 'Something went wrong' });
+    }
+}
+);
+app.get('/posts', async (req, res) => {
+    try {
+        const posts = await prisma.post.findMany({
+            include: {
+                author: true
+            }
+        });
+        return res.json(posts);
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ error: 'Something went wrong' });
+    }
+}
+);
+
+
+
+
+
+
+
 
 
 app.listen(3004, () => {
